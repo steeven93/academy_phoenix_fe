4eae855b0c1df52bd0befd5fb06dcd8ddcbb05f8
Steeven Bolivar Quijije Lacerna <squijijelac@alteanet.it>
2022-05-17 14:15:33 +0200

    #changes view

--- src/components/sections/Header.vue
+++ src/components/sections/Header.vue
@@ -1,78 +1,32 @@
 <script setup>
 import { Popover, PopoverButton, PopoverGroup, PopoverPanel } from '@headlessui/vue'
 import {
-  BookmarkAltIcon,
-  CalendarIcon,
-  ChartBarIcon,
-  CursorClickIcon,
-  MenuIcon,
-  PhoneIcon,
-  PlayIcon,
-  RefreshIcon,
-  ShieldCheckIcon,
-  SupportIcon,
-  ViewGridIcon,
-  XIcon,
+  UserCircleIcon,
+  DocumentTextIcon,
+  CreditCardIcon
 } from '@heroicons/vue/outline'
 import { ChevronDownIcon } from '@heroicons/vue/solid'
 import { useRoute } from 'vue-router';
 
-const solutions = [
+const profile_menu = [
   {
-    name: 'Analytics',
+    name: 'Profile',
     description: 'Get a better understanding of where your traffic is coming from.',
     href: '#',
-    icon: ChartBarIcon,
+    icon: UserCircleIcon,
   },
   {
-    name: 'Engagement',
+    name: 'Invoice',
     description: 'Speak directly to your customers in a more meaningful way.',
     href: '#',
-    icon: CursorClickIcon,
+    icon: DocumentTextIcon,
   },
-  { name: 'Security', description: "Your customers' data will be safe and secure.", href: '#', icon: ShieldCheckIcon },
   {
-    name: 'Integrations',
+    name: 'Your Plans',
     description: "Connect with third-party tools that you're already using.",
     href: '#',
-    icon: ViewGridIcon,
-  },
-  {
-    name: 'Automations',
-    description: 'Build strategic funnels that will drive your customers to convert',
-    href: '#',
-    icon: RefreshIcon,
-  },
-]
-const callsToAction = [
-  { name: 'Watch Demo', href: '#', icon: PlayIcon },
-  { name: 'Contact Sales', href: '#', icon: PhoneIcon },
-]
-const resources = [
-  {
-    name: 'Help Center',
-    description: 'Get all of your questions answered in our forums or contact support.',
-    href: '#',
-    icon: SupportIcon,
-  },
-  {
-    name: 'Guides',
-    description: 'Learn how to maximize our platform to get the most out of it.',
-    href: '#',
-    icon: BookmarkAltIcon,
-  },
-  {
-    name: 'Events',
-    description: 'See what meet-ups and other events we might be planning near you.',
-    href: '#',
-    icon: CalendarIcon,
-  },
-  { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#', icon: ShieldCheckIcon },
-]
-const recentPosts = [
-  { id: 1, name: 'Boost your conversion rate', href: '#' },
-  { id: 2, name: 'How to use search engine optimization to drive traffic to your site', href: '#' },
-  { id: 3, name: 'Improve your customer experience', href: '#' },
+    icon: CreditCardIcon,
+  }
 ]
 
 const route = useRoute()
@@ -86,10 +40,11 @@ const route = useRoute()
       <div class="nav-bar-container">
         <div class="logo-container">
           <a href="#">
-            <span class="sr-only">Workflow</span>
+            <span class="sr-only">Phoenix</span>
             <img class="logo" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
           </a>
         </div>
+
         <div class="menu-mobile">
           <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
             <span class="sr-only">Open menu</span>
@@ -97,92 +52,40 @@ const route = useRoute()
           </PopoverButton>
         </div>
         <PopoverGroup as="nav" class="nav-bar-menu">
-          <Popover class="relative" v-slot="{ open }">
-            <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-500', 'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']">
-              <span>Solutions</span>
-              <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />
-            </PopoverButton>
-
-            <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
-              <PopoverPanel class="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                    <a v-for="item in solutions" :key="item.name" :href="item.href" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                      <component :is="item.icon" class="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                      <div class="ml-4">
-                        <p class="text-base font-medium text-gray-900">
-                          {{ item.name }}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-500">
-                          {{ item.description }}
-                        </p>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                    <div v-for="item in callsToAction" :key="item.name" class="flow-root">
-                      <a :href="item.href" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
-                        <component :is="item.icon" class="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
-                        <span class="ml-3">{{ item.name }}</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </PopoverPanel>
-            </transition>
-          </Popover>
-
-          <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"> Pricing </a>
-          <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"> Docs </a>
-
-          <Popover class="relative" v-slot="{ open }">
-            <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-500', 'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']">
-              <span>More</span>
-              <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />
-            </PopoverButton>
-
-            <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
-              <PopoverPanel class="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0">
-                <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                    <a v-for="item in resources" :key="item.name" :href="item.href" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                      <component :is="item.icon" class="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                      <div class="ml-4">
-                        <p class="text-base font-medium text-gray-900">
-                          {{ item.name }}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-500">
-                          {{ item.description }}
-                        </p>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="px-5 py-5 bg-gray-50 sm:px-8 sm:py-8">
-                    <div>
-                      <h3 class="text-sm tracking-wide font-medium text-gray-500 uppercase">Recent Posts</h3>
-                      <ul role="list" class="mt-4 space-y-4">
-                        <li v-for="post in recentPosts" :key="post.id" class="text-base truncate">
-                          <a :href="post.href" class="font-medium text-gray-900 hover:text-gray-700">
-                            {{ post.name }}
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="mt-5 text-sm">
-                      <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> View all posts <span aria-hidden="true">&rarr;</span></a>
-                    </div>
-                  </div>
-                </div>
-              </PopoverPanel>
-            </transition>
-          </Popover>
+          <router-link class="text-base font-medium text-gray-500 hover:text-gray-900" to="/dashboard">Dashboard</router-link>
         </PopoverGroup>
         <div class="nav-bar-profile-container">
           <div class="flex space-x-2">
-            <div class="relative w-12 h-12">
-              <img class="rounded-full border border-gray-100 shadow-sm" src="https://randomuser.me/api/portraits/women/81.jpg" alt="user image" />
-              <div class="absolute top-0 right-0 h-3 w-3 my-1 border-2 border-white rounded-full bg-green-400 z-2"></div>
-            </div>
+            <PopoverGroup as="nav" class="nav-bar-menu">
+              <Popover class="relative" v-slot="{ open }">
+                <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-500', 'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']">
+                  <div class="relative w-12 h-12">
+                    <img class="rounded-full border border-gray-100 shadow-sm" src="https://randomuser.me/api/portraits/women/81.jpg" alt="user image" />
+                    <div class="absolute top-0 right-0 h-3 w-3 my-1 border-2 border-white rounded-full bg-green-400 z-2"></div>
+                  </div>
+                  <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />
+                </PopoverButton>
+                <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
+                  <PopoverPanel class="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:-left-15 lg:-translate-x-full">
+                    <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                      <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                        <a v-for="item in profile_menu" :key="item.name" :href="item.href" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                          <component :is="item.icon" class="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                          <div class="ml-4">
+                            <p class="text-base font-medium text-gray-900">
+                              {{ item.name }}
+                            </p>
+                            <p class="mt-1 text-sm text-gray-500">
+                              {{ item.description }}
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  </PopoverPanel>
+                </transition>
+              </Popover>
+            </PopoverGroup>
           </div>
         </div>
       </div>
@@ -205,7 +108,7 @@ const route = useRoute()
             </div>
             <div class="mt-6">
               <nav class="grid gap-y-8">
-                <a v-for="item in solutions" :key="item.name" :href="item.href" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                <a v-for="item in profile_menu" :key="item.name" :href="item.href" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
                   <component :is="item.icon" class="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
                   <span class="ml-3 text-base font-medium text-gray-900">
                     {{ item.name }}
--- src/views/CustomerPage.vue
+++ src/views/CustomerPage.vue
@@ -5,6 +5,7 @@
     DisclosurePanel,
   } from '@headlessui/vue'
   import { ChevronRightIcon } from '@heroicons/vue/solid'
+  import { RouterLink } from 'vue-router';
   const open = true
 </script>
 <template>
@@ -21,37 +22,57 @@
                     </div>
                 </div>
                 <div class="notes">
-                    <h1>Your Notes</h1>
-                        <Disclosure v-slot="{ open }">
-                            <DisclosureButton class="fisarmonic-notes py-2">
-                                <span>Is team pricing available?</span>
-                                <!-- Use the `open` slot prop to rotate the icon when the panel is open -->
-                                <ChevronRightIcon :class='open ? "transform rotate-90" : ""' />
-                            </DisclosureButton>
-                            <DisclosurePanel class="description-notes">
-                                ciao
-                            </DisclosurePanel>
-                        </Disclosure>
-                        <Disclosure v-slot="{ open }">
-                            <DisclosureButton class="fisarmonic-notes py-2">
-                                <span>Is team pricing available?</span>
-                                <!-- Use the `open` slot prop to rotate the icon when the panel is open -->
-                                <ChevronRightIcon :class='open ? "transform rotate-90" : ""' />
-                            </DisclosureButton>
-                            <DisclosurePanel class="description-notes">
-                                ciao
-                            </DisclosurePanel>
-                        </Disclosure>
-                        <Disclosure v-slot="{ open }">
-                            <DisclosureButton class="fisarmonic-notes py-2">
-                                <span>Is team pricing available?</span>
-                                <!-- Use the `open` slot prop to rotate the icon when the panel is open -->
-                                <ChevronRightIcon :class='open ? "transform rotate-90" : ""' />
-                            </DisclosureButton>
-                            <DisclosurePanel class="description-notes">
-                                ciao
-                            </DisclosurePanel>
-                        </Disclosure>
+                    <div class="notes-header">
+                        <h1>Your Notes</h1>
+                        <button class="add-note">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+                            </svg>
+                        </button>
+                    </div>
+                    <Disclosure v-slot="{ open }">
+                        <DisclosureButton class="fisarmonic-notes py-2">
+                            <span>Is team pricing available?</span>
+                            <!-- Use the `open` slot prop to rotate the icon when the panel is open -->
+                            <ChevronRightIcon :class='open ? "transform rotate-90" : ""' />
+                        </DisclosureButton>
+                        <DisclosurePanel class="description-notes">
+                            <div class="controls-notes">
+                                <router-link to="/">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                                        <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                                    </svg>
+                                </router-link>
+                                <router-link to="/">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                    </svg>
+                                </router-link>
+                            </div>
+                            ciao
+                        </DisclosurePanel>
+                    </Disclosure>
+                    <Disclosure v-slot="{ open }">
+                        <DisclosureButton class="fisarmonic-notes py-2">
+                            <span>Is team pricing available?</span>
+                            <!-- Use the `open` slot prop to rotate the icon when the panel is open -->
+                            <ChevronRightIcon :class='open ? "transform rotate-90" : ""' />
+                        </DisclosureButton>
+                        <DisclosurePanel class="description-notes">
+                            ciao
+                        </DisclosurePanel>
+                    </Disclosure>
+                    <Disclosure v-slot="{ open }">
+                        <DisclosureButton class="fisarmonic-notes py-2">
+                            <span>Is team pricing available?</span>
+                            <!-- Use the `open` slot prop to rotate the icon when the panel is open -->
+                            <ChevronRightIcon :class='open ? "transform rotate-90" : ""' />
+                        </DisclosureButton>
+                        <DisclosurePanel class="description-notes">
+                            ciao
+                        </DisclosurePanel>
+                    </Disclosure>
                 </div>
             </div>
         </div>
